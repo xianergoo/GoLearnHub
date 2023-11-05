c8a045fa85a9ef2cac269f1a184bd404dadca3cc
zhou <xianergoo@gmail.com>
2023-11-05 13:14:28 +0800

    data struct demo

--- src/p3_datastrcut.go
+++ src/p3_datastrcut.go
@@ -19,10 +19,50 @@ func fibonacci(n int) []int {
 	return res
 }
 
+func romanConvert(s string) int {
+	roman_maps := map[string]int{
+		"M": 1000,
+		"D": 500,
+		"C": 100,
+		"L": 50,
+		"X": 10,
+		"V": 5,
+		"I": 1,
+	}
+	// fmt.Printf("roman_maps: %v\n", roman_maps)
+	arabicVals := make([]int, len(s)+1)
+
+	for index, digit := range s {
+		if val := roman_maps[string(digit)]; val > 0 {
+			arabicVals[index] = val
+		} else {
+			fmt.Printf("Error: The roman numeral %s has a bad digit: %c\n", s, digit)
+			return 0
+		}
+	}
+
+	result := 0
+	for i := 0; i < len(arabicVals)-1; i++ {
+		if arabicVals[i] < arabicVals[i+1] {
+			arabicVals[i] = -arabicVals[i]
+		}
+		result += arabicVals[i]
+	}
+	return result
+
+}
+
 func main() {
 	x := 6
 	s := fibonacci(x)
 	fmt.Printf("s: %v\n", s)
 	// fmt.Printf("s: %v\n", s)
+
+	//convert roman to int
+	roman_number := ""
+	fmt.Println("please input a roman number: ")
+	fmt.Scanln(&roman_number)
+	roman_number2 := romanConvert(roman_number)
+	fmt.Printf("roman_number2: %v\n", roman_number2)
 
 }
