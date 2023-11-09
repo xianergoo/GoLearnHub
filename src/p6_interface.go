4c34619b6641fd33e65a8935af65bd14fbb7cedd
zhou <xianergoo@gmail.com>
2023-11-09 15:01:12 +0800

    interface sample

--- src/p6_interface.go
+++ src/p6_interface.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
 // /* 定义接口 */
 // type interface_name interface {
 // 	method_name1 [return_type]
@@ -23,6 +28,64 @@ package main
 // 	/* 方法实现*/
 //  }
 
+type Account struct {
+	firstName string
+	lastName  string
+}
+
+type EmployeeA struct {
+	Account
+	credits float32
+}
+
+func (a *Account) ChangeName(name string) {
+	a.firstName = name
+}
+
+func (e EmployeeA) String() string {
+	return fmt.Sprintf("name: %s %s\nCreits: %.2f\n", e.firstName, e.lastName, e.credits)
+}
+
+func (e *EmployeeA) AddCredits(amount float32) (float32, error) {
+	if amount > 0.0 {
+		e.credits += amount
+		return e.credits, nil
+	}
+	return 0.0, errors.New("Invalid credit amount.")
+}
+
+func (e *EmployeeA) RemoveCredits(amount float32) (float32, error) {
+	if amount > 0.0 {
+		if amount < e.credits {
+			e.credits -= amount
+			return e.credits, nil
+		}
+		return 0.0, errors.New("You can't remove more credits than the account has.")
+	}
+	return 0.0, errors.New("Invalid credit amount.")
+}
+
+func (e EmployeeA) CheckCredits() float32 {
+	return e.credits
+}
+
 func main() {
+	emp1 := EmployeeA{Account{"john", "snow"}, 400}
+	fmt.Printf("emp1: %v\n", emp1)
+	fmt.Printf("emp1 account: %v\n", emp1.CheckCredits())
+	credits, error := emp1.AddCredits(200)
+	if error != nil {
+		fmt.Println("error")
+	} else {
+		fmt.Printf("credits: %v\n", credits)
+	}
+	fmt.Printf("emp1: %v\n", emp1)
+	_, err := emp1.RemoveCredits(1000)
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+	}
+	fmt.Printf("emp1: %v\n", emp1)
+	emp1.ChangeName("mike")
+	fmt.Printf("emp1: %v\n", emp1)
 
 }
