027dfe85138d43bc5edb2251ff042033207c556e
zhou <noah.zhou@igarment.net>
2024-08-13 14:13:39 +0800

    algo 53/209

---
+++ algorithm/209_minimum_subarray/minimum_subarray_test.go
@@ -0,0 +1,24 @@
+package minimum_subarray
+
+import "testing"
+
+func Test_minSubArrayLen(t *testing.T) {
+	type args struct {
+		target int
+		nums   []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minSubArrayLen(tt.args.target, tt.args.nums); got != tt.want {
+				t.Errorf("minSubArrayLen() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
