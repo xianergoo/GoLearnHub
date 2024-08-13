8dece32438c78268b7bdbcd9baad507ac6694376
zhou <noah.zhou@igarment.net>
2024-08-13 13:38:52 +0800

    generate matrix

--- algorithm/59_spiral_matrix_ii/spiral_matrix_ii.go
+++ algorithm/59_spiral_matrix_ii/spiral_matrix_ii.go
@@ -1,35 +1,79 @@
 package spiral_matrix_ii
 
+import "fmt"
+
 func generateMatrix(n int) [][]int {
 	left, right := 0, n-1
 	top, bottom := 0, n-1
-	fornum := 0
+	fornum := 1
 	target_num := n * n
 	res := make([][]int, n)
 	for i := 0; i < n; i++ {
 		res[i] = make([]int, n)
 	}
-	for fornum < target_num {
-		for i := left; i < right; i++ {
+	for fornum <= target_num {
+		for i := left; i <= right; i++ {
 			res[top][i] = fornum
 			fornum++
+			fmt.Println(res)
 		}
-		for i := top; i < bottom; i++ {
-			res[right][i] = fornum
+		top++
+		for i := top; i <= bottom; i++ {
+			res[i][right] = fornum
 			fornum++
+			fmt.Println(res)
 		}
-		for i := right; i > left; i-- {
+		right--
+		for i := right; i >= left; i-- {
 			res[bottom][i] = fornum
 			fornum++
+			fmt.Println(res)
 		}
-		for i := bottom; i > top; i-- {
-			res[left][i] = fornum
+		bottom--
+		for i := bottom; i >= top; i-- {
+			res[i][left] = fornum
 			fornum++
+			fmt.Println(res)
 		}
 		left++
-		top++
-		right--
-		bottom--
 	}
 	return res
+}
+
+func Add(a int, b int) int {
+	return a + b
+}
+
+func generateMatrix2(n int) [][]int {
+	top, bottom := 0, n-1
+	left, right := 0, n-1
+	num := 1
+	tar := n * n
+	matrix := make([][]int, n)
+	for i := 0; i < n; i++ {
+		matrix[i] = make([]int, n)
+	}
+	for num <= tar {
+		for i := left; i <= right; i++ {
+			matrix[top][i] = num
+			num++
+		}
+		top++
+		for i := top; i <= bottom; i++ {
+			matrix[i][right] = num
+			num++
+		}
+		right--
+		for i := right; i >= left; i-- {
+			matrix[bottom][i] = num
+			num++
+		}
+		bottom--
+		for i := bottom; i >= top; i-- {
+			matrix[i][left] = num
+			num++
+		}
+		left++
+	}
+	return matrix
 }
--- algorithm/59_spiral_matrix_ii/spiral_matrix_ii_test.go
+++ algorithm/59_spiral_matrix_ii/spiral_matrix_ii_test.go
@@ -5,20 +5,64 @@ import (
 	"testing"
 )
 
-func TestGenerateMatrix(t *testing.T) {
+func TestAdd(t *testing.T) {
 	cases := []struct {
-		n        int
-		expected [][]int
+		a        int
+		b        int
+		expected int
 	}{
-		{3, [][]int{{1, 2, 3}, {8, 9, 4}, {7, 6, 5}}},
-		{1, [][]int{{1}}},
+		{1, 2, 3},
+		{3, 4, 7},
 	}
 
 	for _, c := range cases {
-		actual := generateMatrix(c.n)
-		if !reflect.DeepEqual(actual, c.expected) {
-			t.Errorf("For input %d, expected %v, but got %v", c.n, c.expected, actual)
+		expected := Add(c.a, c.b)
+		if expected != c.expected {
+			t.Errorf("%d + %d expected be %d, but %d got", c.a, c.b, c.expected, expected)
 		}
 	}
+}
 
+func Test_generateMatrix(t *testing.T) {
+	type args struct {
+		n int
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{"test1", args{3}, [][]int{{1, 2, 3}, {8, 9, 4}, {7, 6, 5}}},
+		{"test1", args{1}, [][]int{{1}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := generateMatrix(tt.args.n); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("generateMatrix(%v) = %v, want %v", tt.args.n, got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_generateMatrix2(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{"test1", args{3}, [][]int{{1, 2, 3}, {8, 9, 4}, {7, 6, 5}}},
+		{"test1", args{1}, [][]int{{1}}},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := generateMatrix2(tt.args.n); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("generateMatrix2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
